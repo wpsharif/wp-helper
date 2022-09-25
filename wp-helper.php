b6f383a156dd3f9c743dfe6e5a66fec3549df0b0
Md Sharif Miah <sharif.golang@gmail.com>
2022-09-25 21:10:50 +0600

    Plugin directory structure done

---
+++ wp-helper.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name:       WP Helper
+ * Plugin URI:        https://wordpress.org/
+ * Description:       Make minimal, lightweight WordPress webapp helper.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            wpsharif
+ * Author URI:        https://wordpress.org/
+ * License:           GPL v2 or later
+ * Text Domain:       wp-helper
+ * Domain Path:       /languages
+ */
